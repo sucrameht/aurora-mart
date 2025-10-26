b3805c6fea972c560774c2a9634a15d3c01deb91
Marcus Lim <marcuslim136@gmail.com>
2025-10-27 00:10:42 +0800

    Edited storefront to storefront_home

--- aurora_mart_proj/storefront/urls.py
+++ aurora_mart_proj/storefront/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.storefront, name='storefront'),
+    path('', views.storefront, name='storefront_home'),
     path('cart/', views.view_cart, name='view_cart'),
     path('cart/add/', views.add_to_cart, name='add_to_cart'),
     path('cart/update/', views.update_cart, name='update_cart'),
